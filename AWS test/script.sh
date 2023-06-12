dca45c71e3037796fa5f69ff920e7838fbb20a68
rverspaij <103568731+rverspaij@users.noreply.github.com>
2023-06-12 10:04:53 +0200

    Update script.sh

--- AWS test/script.sh
+++ AWS test/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IP_ADDRESS="10.0.2.20"
-PORT="55000"
+PORT="1514"
 
 function check_port() {
     nc -zv "$IP_ADDRESS" "$PORT" >/dev/null 2>&1
@@ -13,10 +13,11 @@ while ! check_port; do
     sleep 1
 done
 
-echo "The ping to $IP_ADDRESS on port $PORt!" >> /var/log/logfile.txt
+echo "The ping to $IP_ADDRESS on port $PORT!" >> /var/log/logfile.txt
 
 curl -so wazuh-agent.deb https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.4.3-1_amd64.deb && sudo WAZUH_MANAGER='10.0.2.20' WAZUH_AGENT_GROUP='default' WAZUH_AGENT_NAME='Test' dpkg -i ./wazuh-agent.deb
 sudo systemctl daemon-reload
 sudo systemctl enable wazuh-agent
+sudo systemctl start wazuh-agent
 
 echo "The installation was successful!" >> /var/log/logfile.txt
